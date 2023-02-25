f94e6974152af1a1ff67f2a480fcbbac99ab3542
BrayanAlaya <brayan.alaya@hotmail.com>
2023-02-25 13:14:24 -0500

    V1 boletos agregados

--- build/js/app.js
+++ build/js/app.js
@@ -15,6 +15,43 @@ function crearGaleria(){
         <source srcset="./build/img/thumb/${i}.webp" type="image/webp">
         <img width="300" height="200" loading="lazy" src="./build/img/thumb/${i}.jpg" alt="imagen de galeria">
         `;
+        imagenes.onclick = function(){
+            mostrarImagen(i);
+        }
         galeria.appendChild(imagenes);
     }
 }
+
+function mostrarImagen(id){
+    const imagenes = document.createElement("picture");
+        imagenes.innerHTML = `
+        <source srcset="./build/img/grande/${id}.avif" type="image/avif">
+        <source srcset="./build/img/grande/${id}.webp" type="image/webp">
+        <img width="300" height="200" loading="lazy" src="./build/img/grande/${id}.jpg" alt="imagen de galeria">
+        `;
+
+    //Crear imagen
+    const overley = document.createElement("div")
+    overley.appendChild(imagenes);
+    overley.onclick = function(){
+        body.classList.remove("fijar-body")
+        overley.remove();
+    }
+    overley.classList.add("overlay")
+
+    //crear boton cerrar
+    const cerrarModal = document.createElement("p");
+    cerrarModal.classList.add("btn-cerrar")
+    cerrarModal.textContent = "X";
+    cerrarModal.onclick = function(){
+        body.classList.remove("fijar-body")
+        overley.remove();
+    }
+    overley.appendChild(cerrarModal);
+
+    //agregar overlay al body
+    const body = document.querySelector("body")
+    body.classList.add("fijar-body")
+    body.appendChild(overley)
+}
+
